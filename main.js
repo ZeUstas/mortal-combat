6af7d13e6ee08be928a91579b4afc69935c8bc18
Ustas <ustas0001@gmail.com>
2021-04-08 18:08:02 +0300

    add createPlayer() (Task#1)

--- main.js
+++ main.js
@@ -17,3 +17,36 @@ const player2 = {
     console.log(this.name + 'Fight...');
   },
 };
+
+function createPlayer(playerNodeClassName, playerName, playerHitPoints) {
+  const $player = document.createElement('div');
+  $player.className = playerNodeClassName;
+
+  const $progressBar = document.createElement('div');
+  $progressBar.className = 'progressbar';
+
+  const $life = document.createElement('div');
+  $life.className = 'life';
+  $life.style.width = '100%';
+  // $life.innerText = playerHitPoints;
+
+  const $name = document.createElement('div');
+  $name.className = 'name';
+  $name.innerText = playerName;
+
+  const $character = document.createElement('div');
+  $character.className = 'character';
+
+  const $image = document.createElement('img');
+  $image.src = 'http://reactmarathon-api.herokuapp.com/assets/scorpion.gif';
+
+  document.querySelector('.arenas').appendChild($player);
+
+  $player.appendChild($progressBar);
+  $player.appendChild($character);
+
+  $progressBar.appendChild($life);
+  $progressBar.appendChild($name);
+
+  $character.appendChild($image);
+}
