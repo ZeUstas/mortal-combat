358f2468dafc43d4666766f589e6c0348c11346b
Ustas <ustas0001@gmail.com>
2021-04-28 01:04:42 +0300

    refactor classes and files structure

---
+++ Player/index.js
@@ -0,0 +1,56 @@
+import { createElement } from '../utils/index.js';
+
+export class Player {
+  constructor(props) {
+    this.name = props.name;
+    this.hp = props.hp;
+    this.img = props.img;
+    this.indexNum = props.indexNum;
+    this.selector = `player${this.indexNum}`;
+    this.rootSelector = props.rootSelector;
+  }
+
+  changeHp = (num) => {
+    this.hp += num;
+    if (this.hp < 0) {
+      this.hp = 0;
+    }
+  }
+
+  elHp = () => {
+    const $node = document.querySelector(`.${this.selector} .life`);
+    return $node;
+  }
+
+  renderHp = () => {
+    this.elHp().style.width = `${this.hp}%`;
+  }
+
+  create = () => {
+    const $player = createElement('div', this.selector);
+    const $progressBar = createElement('div', 'progressbar');
+    const $life = createElement('div', 'life');
+    const $name = createElement('div', 'name');
+    const $character = createElement('div', 'character');
+    const $image = createElement('img');
+
+    $life.style.width = `${this.hp}%`;
+    $name.innerText = this.name;
+    $image.src = this.img;
+
+    $player.appendChild($progressBar);
+    $player.appendChild($character);
+    $progressBar.appendChild($life);
+    $progressBar.appendChild($name);
+    $character.appendChild($image);
+
+    return $player;
+  }
+
+  enterArena = () => {
+    const $root = document.querySelector(`.${this.rootSelector}`);
+    $root.appendChild(this.create());
+  }
+}
+
+// export default Player;
